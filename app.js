e5873acc03960472de714a5537569ed57f8e8707
Iliya Iliev <iliya.vasilev.iliev@gmail.com>
2016-03-30 15:23:43 +0300

    a

--- app.js
+++ app.js
@@ -11,13 +11,14 @@ var server = http.createServer(function (req, res) {
     url += 'index.html';
 
 
-    res.json(env);
+    console.log(env);
   }
 
   // IMPORTANT: Your application HAS to respond to GET /health with status 200
   //            for OpenShift health monitoring
 
   if (url == '/health') {
+    res.json(env);
     res.writeHead(200);
     res.end();
   } else if (url.indexOf('/info/') == 0) {
