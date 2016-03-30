a65979f3a9ee67f9e1f5863c0160170fa6dbb5c4
Iliya Iliev <iliya.vasilev.iliev@gmail.com>
2016-03-30 19:06:02 +0300

    ip host address

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/static'));
 
-app.get('/process', function(req, res){
-  res.json(process.env);
-});
 
 var ip = process.env.OPENSHIFT_NODEJS_IP || 'localhost';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var mongoUrl = process.envOPENSHIFT_MONGODB_DB_URL;
+var connectionUrl = mongoUrl || 'mongodb://localhost/test';
 
+mongoose.connect(connectionUrl);
+
+app.get('/process', function(req, res){
+  res.json(process.env);
+});
 app.listen(port, ip);
