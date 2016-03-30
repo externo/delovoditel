87e931027ed657d0e75484968b1e2d08ddc64947
Iliya Iliev <iliya.vasilev.iliev@gmail.com>
2016-03-30 13:42:29 +0300

    as

---
+++ static/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular
+  .module('app', ['ngRoute'])
+  .config(function ($routeProvider) {
+    $routeProvider.when('/admin/case', {
+      templateUrl: 'states/admin/admin.html',
+      controller: 'AdminController'
+    });
+    $routeProvider.otherwise(
+      {redirectTo: '/admin/case'}
+    );
+  });
