edcaf7453df7aabe5af5290eecbb8d471168bcd4
Iliya Iliev <iliya.vasilev.iliev@gmail.com>
2016-03-30 21:09:42 +0300

    a

--- static/states/admin/admin.js
+++ static/states/admin/admin.js
@@ -24,7 +24,7 @@ function AdminController($http, $scope, CaseService, FileService) {
 
   Admin.editCase = function () {
     console.log('id: ' + Admin.currentCase._id);
-    console.log('cc: ' + Admin.currentCase);
+    console.log('cc: ' + JSON.stringify(Admin.currentCase));
     $http.put('/admin/case/' + Admin.currentCase._id, Admin.currentCase)
       .then(function (res) {
         Admin.cases = res.data;
