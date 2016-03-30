46d5bf52626f6da8075b0065dd9c8ec3f8b2af26
Iliya Iliev <iliya.vasilev.iliev@gmail.com>
2016-03-30 13:46:23 +0300

    as

---
+++ static/states/admin/admin.js
@@ -0,0 +1,83 @@
+'use strict';
+
+angular
+  .module('app')
+  .controller('AdminController', AdminController);
+
+function AdminController($http, $scope, CaseService, FileService) {
+
+  var Admin = this;
+
+  Admin.currentCase = null;
+  Admin.newcase = null;
+  Admin.click = false;
+  Admin.newfile = null;
+
+  Admin.addCase = function (newcase) {
+    newcase.files = [];
+    $http.post('/admin/case', newcase)
+      .then(function (res) {
+        Admin.cases = res.data;
+      }
+    );
+  };
+
+  Admin.editCase = function (currentCase) {
+    $http.put('/admin/case/' + currentCase._id, currentCase)
+      .then(function (res) {
+        Admin.cases = res.data;
+        Admin.click = false;
+      }
+    );
+  };
+
+  Admin.getCase = function (id) {
+    $http.get('/admin/case/' + id)
+      .then(function (res) {
+        Admin.currentCase = res.data;
+        Admin.click = true;
+      }
+    );
+  };
+
+  Admin.removeCase = function (id) {
+    $http.delete('/admin/case/' + id)
+      .then(function (res) {
+        Admin.cases = res.data;
+      }
+    );
+  };
+
+  Admin.uploadFile = function (file) {
+    var fd = new FormData();
+    fd.append('file', file);
+
+    $http.post('/file', fd, {
+      transformRequest: angular.identity,
+      headers: {
+        'Content-Type': undefined,
+        filename: file.name
+      }
+    })
+      .then(function (res) {
+        Admin.currentCase.files.push(res.data);
+      });
+  };
+
+  Admin.removeFile = function (id) {
+    $http.delete('/file/' + id)
+      .then(function (res) {
+        var fileIds = Admin.currentCase.files;
+        var idIndex = fileIds.indexOf(res.data);
+        fileIds.splice(idIndex, 1);
+      });
+  };
+
+  CaseService.findAll(function (response) {
+    Admin.cases = response;
+  });
+
+  FileService.find(function (response) {
+    Admin.files = response;
+  });
+}
