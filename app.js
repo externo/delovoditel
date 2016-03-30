8248a7cc8bf01b0d231fb2bc02066a17dbd639b4
Iliya Iliev <iliya.vasilev.iliev@gmail.com>
2016-03-30 21:18:41 +0300

    a

--- app.js
+++ app.js
@@ -55,21 +55,9 @@ app.get('/admin/case/:id', function (req, res) {
 
 app.put('/admin/case/:id', function (req, res) {
   var caseId = new mongo.ObjectID(req.params.id);
-  console.log('r b ' + JSON.stringify(req.body));
   db.collection('cases').updateOne(
     {_id: caseId},
-    {
-      $set: {
-        "number": req.body.number,
-        "court": req.body.court,
-        "instance": req.body.instance,
-        "type": req.body.type,
-        "client.name": req.body.client.name,
-        "client.phone": req.body.client.phone,
-        "files": req.body.files
-      },
-      $currentDate: {"lastModified": true}
-    },
+    req.body,
     function (err, results) {
       db.collection('cases').find()
         .toArray(function (err, cases) {
