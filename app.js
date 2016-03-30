437730f93e0eae8181a74890da829a50084e4868
Iliya Iliev <iliya.vasilev.iliev@gmail.com>
2016-03-30 16:05:08 +0300

    a

--- app.js
+++ app.js
@@ -1,20 +1,158 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
+'use strict';
 
+const env = process.env;
+var ip = env.NODE_IP || 'localhost';
+var port = env.NODE_PORT || 3000;
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongo = require('mongodb');
+var MongoClient = mongo.MongoClient;
+var Grid = require('gridfs-stream');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + '/static'));
 
+var db;
+var gfs;
 var connectionUrl = 'mongodb://' + process.env.OPENSHIFT_MONGODB_DB_HOST + ':27017/admin' || 'mongodb://localhost/test';
-mongoose.connect(connectionUrl);
 
-app.get('/process', function(req, res){
-  res.json(process.env);
+// Initialize connection once
+MongoClient.connect(connectionUrl, function(err, database) {
+  if(err) throw err;
+
+  db = database;
+  gfs = Grid(db, mongo); // mongodb cursor
+
+  // Start the application after the database connection is ready
+  app.listen(port);
+  //console.log("env: " + JSON.stringify(env));
 });
 
-var ip = process.env.OPENSHIFT_NODEJS_IP || 'localhost';
-var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+app.get('/admin/case', function (req, res) {
+  db.collection('cases').find()
+    .toArray(function (err, cases) {
+      res.json(cases);
+    }
+  )
+});
 
-app.listen(port, ip);
+app.post('/admin/case', function (req, res) {
+  db.collection('cases').insertOne(
+    req.body,
+    function (err, result) {
+      db.collection('cases').find()
+        .toArray(function (err, cases) {
+          res.json(cases);
+        }
+      );
+    });
+});
+
+app.get('/admin/case/:id', function (req, res) {
+  var caseId = new mongo.ObjectID(req.params.id);
+  console.log(req.params.id);
+  db.collection('cases').findOne({_id: caseId}, function (err, doc) {
+    res.json(doc);
+  });
+});
+
+app.put('/admin/case/:id', function (req, res) {
+  var caseId = new mongo.ObjectID(req.params.id);
+  console.log('r b ' + JSON.stringify(req.body));
+  db.collection('cases').updateOne(
+    {_id: caseId},
+    {
+      $set: {
+        "number": req.body.number,
+        "court": req.body.court,
+        "instance": req.body.instance,
+        "type": req.body.type,
+        "client.name": req.body.client.name,
+        "client.phone": req.body.client.phone,
+        "files": req.body.files
+      },
+      $currentDate: {"lastModified": true}
+    },
+    function (err, results) {
+      db.collection('cases').find()
+        .toArray(function (err, cases) {
+          res.json(cases);
+        }
+      );
+    });
+});
+
+app.delete('/admin/case/:id', function (req, res) {
+  var caseId = new mongo.ObjectID(req.params.id);
+  db.collection('cases').deleteOne(
+    {_id: caseId},
+    function (err, results) {
+      db.collection('cases').find()
+        .toArray(function (err, cases) {
+          res.json(cases);
+        }
+      );
+    }
+  );
+});
+
+app.get('/admin/case/:id/file', function (req, res) {
+  gfs.files.find()
+    .map(function (doc) {
+      return {
+        id: doc._id,
+        name: doc.filename
+      };
+    })
+    .toArray(function (err, files) {
+      res.send(files);
+    });
+});
+
+app.get('/file/:id', function (req, res) {
+  gfs.findOne({_id: req.params.id}, function (err, file) {
+    if (err) return res.status(400).send(err);
+    if (!file) return res.status(404).send('');
+
+    res.set('Content-Type', file.contentType);
+    res.set('Content-Disposition', 'attachment; filename="' + file.filename + '"');
+
+    var readstream = gfs.createReadStream({
+      _id: file._id
+    });
+
+    readstream.on("error", function (err) {
+      console.log("Got error while processing stream " + err.message);
+      res.end();
+    });
+
+    readstream.pipe(res);
+  });
+});
+
+app.post('/file', function (req, res) {
+  var filename = req.headers.filename;
+  var writeStream = gfs.createWriteStream({
+    filename: filename
+  });
+
+  writeStream.on('finish', function () {
+    res.send(writeStream.id);
+  });
+
+  req.pipe(writeStream);
+});
+
+app.delete('/file/:id', function (req, res) {
+  gfs.remove({_id: req.params.id}, function (err, file) {
+    if (err) return res.status(400).send(err);
+    if (!file) return res.status(404).send('');
+
+    res.send(req.params.id);
+  });
+});
+
+//app.listen(port, ip);
