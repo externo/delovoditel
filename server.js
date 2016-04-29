0ec2bc28edfef2703dd70662f43fa9083c683332
Iliya Iliev <iliya.vasilev.iliev@gmail.com>
2016-04-29 08:55:35 +0300

    access-header problem

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var httpServer = http.createServer(app);
 app.use(express.static(__dirname + '/public'));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "'X-Requested-With,content-type'");
+  res.setHeader("Access-Control-Allow-Headers", "'X-Requested-With,content-type'");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   next();
 });
