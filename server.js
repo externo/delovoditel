3fb76cb9e3122cbf63a67ee09a5f48c39d12fc6d
Iliya Iliev <iliya.vasilev.iliev@gmail.com>
2016-04-29 07:59:48 +0300

    implement notification with confirm to calendar

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ var httpServer = http.createServer(app);
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://delovoditel.gq");
   //res.header("Access-Control-Allow-Headers", "'X-Requested-With'");
-  res.header("Access-Control-Allow-Headers", "'Content-Type'");
+  res.setHeader("Access-Control-Allow-Headers", "'Content-Type'");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
 });
